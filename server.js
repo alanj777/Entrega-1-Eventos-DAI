a375851f9037000ac1e9f94959c460eebc13c516
alanj777 <68874997+alanj777@users.noreply.github.com>
2024-04-26 08:35:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import EventController from "./src/controllers/event-controller.js";
+import UserController from "./src/controllers/user-controller.js";
+
+
+
+const app = express(); 
+app.use(express.json()); 
+const port = 3000;
+
+app.use("/event", EventController );
+app.use("/user", UserController );
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
