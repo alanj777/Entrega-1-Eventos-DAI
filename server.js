3816a344e93ff41b6f581c3ca15e4c4458f82cc4
alanj777 <68874997+alanj777@users.noreply.github.com>
2024-05-24 08:24:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 import express from "express";
-import EventController from "./src/controllers/event-controller.js";
-import UserController from "./src/controllers/user-controller.js";
+import EventController from "./controllers/event-controller.js";
+import UserController from "./controllers/user-controller.js";
+import ProvinciasController from "./controllers/provincias-controller.js";
+import LocationController from "./controllers/location-controller.js";
+import CategoryController from "./controllers/category-controller.js";
 
 
-
-const app = express(); 
-app.use(express.json()); 
+const app = express(); // Init API REST
+app.use(express.json()); // Middleware to parse JSON
 const port = 3000;
 
 app.use("/event", EventController );
 app.use("/user", UserController );
+app.use("/provincias", ProvinciasController );  
+app.use("/event-location", LocationController );  
+app.use("/event-category", CategoryController );  
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+
+  
