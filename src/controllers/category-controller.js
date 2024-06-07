f17fb1ed5e2d8c5d2eafc97cc4dcfa3a0649baf4
ALAN NATANIEL JOLODOVSKY <46830988@ort>
2024-06-07 11:16:03 -0300

    Fixed 2

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,5 +1,5 @@
 import express from "express";
-import categoryService from "../servicios/category-service.js";
+import categoryService from "../Servicios/category-service.js";
 
 const router = express.Router();
 
